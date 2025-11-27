452df20e1bb9fdeca7b056426f0e126764cd269c
Muskan-Begum <ayshahmuskan@gmail.com>
2025-11-27 19:05:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Navbar from './components/shared/Navbar'
-import Login from './components/auth/Login'
+import Login from './components/auth/login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
